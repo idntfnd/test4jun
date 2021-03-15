564fbb32839573d962344d6290650f23b450c526
Andrey <idntfnd@users.noreply.github.com>
2021-03-15 20:03:49 +0300

    change jQuery v1

--- assets/script.js
+++ assets/script.js
@@ -6,5 +6,68 @@ btnRequest.onclick = function showRequestMenu() {
 };
 //$('.dropdownselect')
 
-let selector = $('selector')
-console.log(selector)
+$('#profession01').each(function(){
+		// Variables
+		let $this = $(this),
+			selectOption = $this.find('option'),
+			selectOptionLength = selectOption.length,
+			selectedOption = selectOption.filter(':selected'),
+			dur = 100;
+
+		$this.hide();
+		// Wrap all in select box
+		$('#profession01').wrap('<div class="select"></div>');
+		// Style box
+		$('<div>',{
+			class: 'select__gap',
+			text: 'Должность',
+      textAligt: "left",
+      padding: "12"
+		}).
+    insertAfter($this);
+
+		let selectGap = $this.next('.select__gap'),
+			caret = selectGap.find('.caret');
+		// Add ul list
+		$('<ul>',{
+			class: 'select__list'
+		}).insertAfter(selectGap);
+
+		let selectList = selectGap.next('.select__list');
+		// Add li - option items
+		for(let i = 0; i < selectOptionLength; i++){
+			$('<li>',{
+				class: 'select__item',
+				html: $('<span>',{
+					text: selectOption.eq(i).text()
+				})
+			})
+			.attr('data-value', selectOption.eq(i).val())
+			.appendTo(selectList);
+		}
+		// Find all items
+		let selectItem = selectList.find('li');
+
+		selectList.slideUp(0);
+		selectGap.on('click', function(){
+			if(!$(this).hasClass('on')){
+				$(this).addClass('on');
+				selectList.slideDown(dur);
+
+				selectItem.on('click', function(){
+					let chooseItem = $(this).data('value');
+
+					$('select').val(chooseItem).attr('selected', 'selected');
+					selectGap.text($(this).find('span').text());
+
+					selectList.slideUp(dur);
+					selectGap.removeClass('on');
+				});
+
+			} else {
+				$(this).removeClass('on');
+				selectList.slideUp(dur);
+			}
+		});
+
+	});
